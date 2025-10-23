0928964ab5db673b33d4858dc0b1ae57099a24c2
einoh <einoh@TUF-DEVPC>
2025-10-24 00:23:48 +0800

    intial update for printing

--- Chemical_Ne/Dashboard.Designer.cs
+++ Chemical_Ne/Dashboard.Designer.cs
@@ -30,26 +30,39 @@
         {
             this.lblCredits = new System.Windows.Forms.Label();
             this.lblMySQLConStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PdPrinter = new System.Drawing.Printing.PrintDocument();
             this.SuspendLayout();
             // 
             // lblCredits
             // 
             this.lblCredits.AutoSize = true;
-            this.lblCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCredits.Location = new System.Drawing.Point(150, 181);
+            this.lblCredits.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCredits.Location = new System.Drawing.Point(57, 113);
             this.lblCredits.Name = "lblCredits";
-            this.lblCredits.Size = new System.Drawing.Size(198, 37);
+            this.lblCredits.Size = new System.Drawing.Size(294, 56);
             this.lblCredits.TabIndex = 0;
             this.lblCredits.Text = "Retrieving...";
             // 
             // lblMySQLConStatus
             // 
             this.lblMySQLConStatus.AutoSize = true;
-            this.lblMySQLConStatus.Location = new System.Drawing.Point(170, 313);
+            this.lblMySQLConStatus.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMySQLConStatus.Location = new System.Drawing.Point(51, 28);
             this.lblMySQLConStatus.Name = "lblMySQLConStatus";
-            this.lblMySQLConStatus.Size = new System.Drawing.Size(75, 13);
+            this.lblMySQLConStatus.Size = new System.Drawing.Size(107, 19);
             this.lblMySQLConStatus.TabIndex = 1;
             this.lblMySQLConStatus.Text = "MySQL Status";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Dashboard
             // 
@@ -57,6 +70,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1024, 768);
             this.ControlBox = false;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblMySQLConStatus);
             this.Controls.Add(this.lblCredits);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -72,5 +86,7 @@
 
         public System.Windows.Forms.Label lblCredits;
         public System.Windows.Forms.Label lblMySQLConStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Drawing.Printing.PrintDocument PdPrinter;
     }
 }
--- Chemical_Ne/Dashboard.cs
+++ Chemical_Ne/Dashboard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,51 @@ namespace Chemical_Ne
 {
     public partial class Dashboard : Form
     {
+
         public Dashboard()
         {
             InitializeComponent();
+            PdPrinter.PrintPage += PdPrinter_PrintPage;  // Add this line
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //check if lblCredits value is not zero or empty
+            if (!string.IsNullOrEmpty(lblCredits.Text) && lblCredits.Text != "0" && lblCredits.Text != "Retrieving...")
+            {
+                PrintVoucher();
+            }
+            else
+            {
+                MessageBox.Show("No credits available.");
+            }
+        }
+
+        private void PdPrinter_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Font repFontNormal = new Font("Century Gothic", 10);
+            Font repFontVoucher = new Font("Century Gothic", 14, FontStyle.Bold);
+
+            e.Graphics.DrawString("Branchette WiFi", repFontNormal, Brushes.Black, 80, 5);
+            e.Graphics.DrawString("9fasFs15sf", repFontVoucher, Brushes.Black, 80, 25);
+            e.Graphics.DrawString("1 Hour Voucher Code", repFontNormal, Brushes.Black, 70, 50);
+            e.Graphics.DrawString(DateTime.Now.ToString("yyyy-MM-dd hh:mm tt"), repFontNormal, Brushes.Black, 80, 70);
+            e.Graphics.DrawString("Golden Success College", repFontNormal, Brushes.Black, 80, 90);
+        }
+
+        public void PrintVoucher()
+        {
+            try
+            {
+                PdPrinter.PrinterSettings.Copies = 1;
+                PdPrinter.PrintController = new StandardPrintController(); // hides print dialog
+                PdPrinter.Print();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Printing error: {ex.Message}", "Print Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
