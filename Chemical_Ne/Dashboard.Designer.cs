2ceb53d37039d4ff1cde63c01dca626d267e6f67
einoh <einoh@TUF-DEVPC>
2025-10-30 08:48:53 +0800

    fix

--- Chemical_Ne/Dashboard.Designer.cs
+++ Chemical_Ne/Dashboard.Designer.cs
@@ -225,11 +225,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(328, 19);
+            this.label1.Location = new System.Drawing.Point(259, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(417, 56);
+            this.label1.Size = new System.Drawing.Size(518, 56);
             this.label1.TabIndex = 9;
-            this.label1.Text = "BRANCHETTE WIFI";
+            this.label1.Text = "BRANCHETTE SYSTEMS";
             // 
             // lblError
             // 
